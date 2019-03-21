afb33ce0ec4a05e3a067a4f4c784b9ae17be41c5
unknown <timothy.leroch@shadline.com>
2019-03-21 16:45:51 +0100

    change some paths

---
+++ build/precache-manifest.39f5b9e22879097c30f60feced1670b5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "efbacba66f578f914f8ebf67d9e2025e",
+    "url": "./assets/images/brick6.png"
+  },
+  {
+    "revision": "c33996da4fe2e5e20322",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "4869a2f5991a69ff327e",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "8ce239c2db24f0372094d452ceeba2ba",
+    "url": "./assets/images/button.png"
+  },
+  {
+    "revision": "5286aa28276d0a643ba4e5e74b42cc1b",
+    "url": "./assets/images/d.png"
+  },
+  {
+    "revision": "0bf8760752aff25142723cabef8d4ae4",
+    "url": "./assets/images/fireBall.jpg"
+  },
+  {
+    "revision": "f8ba06c323a8fc17c0044df6df948a1f",
+    "url": "./assets/images/phaser-es6-webpack.jpg"
+  },
+  {
+    "revision": "6d79a87ef32b28bed87ae3f7d62bd253",
+    "url": "./assets/images/paddle.png"
+  },
+  {
+    "revision": "78ea0555ef7648bf378481a5d778d344",
+    "url": "./assets/images/loader-bar.png"
+  },
+  {
+    "revision": "dfb749fb41edc5c53626b50ec9d48776",
+    "url": "./assets/images/loader-bg.png"
+  },
+  {
+    "revision": "c97d238339edea4c33afd5a7f6a0d8ce",
+    "url": "./assets/images/icon-512px.png"
+  },
+  {
+    "revision": "c6d61aea87c84a8aee0398300d81986d",
+    "url": "./assets/images/fireBall.png"
+  },
+  {
+    "revision": "6eb0c377e0ea2aac41ac8d8b229a8020",
+    "url": "./assets/images/icon-192px.png"
+  },
+  {
+    "revision": "98e3c5b5c0ccffb933ac5fbead181d23",
+    "url": "./assets/images/d.png - Raccourci.lnk"
+  },
+  {
+    "revision": "470f8bcd66523f7a59d439e827bcb9e7",
+    "url": "./assets/images/defaultBonus.png"
+  },
+  {
+    "revision": "aeafd57781c8e4e2c4437cb4b8736806",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99adcb58314359e9d075ca1c21c6473c",
+    "url": "./assets/images/breakout_pieces.png"
+  },
+  {
+    "revision": "993b8629cd390bef08a8e7e21fc7091b",
+    "url": "./assets/images/bonusExtraBalls.png"
+  },
+  {
+    "revision": "6b12d0f75e53f9a33f4600b05abad8e2",
+    "url": "./assets/images/brick4.png"
+  },
+  {
+    "revision": "9453b5cf07f66cd1072fc141117499bd",
+    "url": "./assets/images/brick5.png"
+  },
+  {
+    "revision": "a6bd53086456898e6ce4ce9efcf5ec36",
+    "url": "./assets/images/brick3.png"
+  },
+  {
+    "revision": "a5da6d1546e9b6c927d237fc914f3487",
+    "url": "./assets/images/brick2.png"
+  },
+  {
+    "revision": "cc738b542a193089c3c4be251ff877fb",
+    "url": "./assets/images/brick1.png"
+  },
+  {
+    "revision": "3d1598801b3329555d29687b95503b9d",
+    "url": "./assets/images/brick7.png"
+  },
+  {
+    "revision": "3500e1b21a6717c72d0c5b96e866d79c",
+    "url": "./assets/images/bonusMaxPaddle.png"
+  },
+  {
+    "revision": "bda97abe7d5a4046dfb754c332000e2e",
+    "url": "./assets/images/bonusLife.png"
+  },
+  {
+    "revision": "e6889cddf61e8ce0576d9ea79f71abb4",
+    "url": "./assets/images/bonusFireBall.png"
+  },
+  {
+    "revision": "3eb6ddbaa5466294d8329fe2066858d9",
+    "url": "./assets/images/breakout_pieces (1).png"
+  },
+  {
+    "revision": "b6db9e847067becf722e54da3c8c99cd",
+    "url": "./assets/images/ball.png"
+  },
+  {
+    "revision": "bb57ae8a7f547311f84722f8106f552d",
+    "url": "././manifest.json"
+  }
+];
