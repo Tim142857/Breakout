884207d6b60e797a938a194817084b1b1a9bc2b9
unknown <timothy.leroch@shadline.com>
2019-03-21 16:11:58 +0100

    change port conf for heroku

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use('/login', (req, res) => {
   res.send(true)
 })
 
-app.listen(3000)
+let port = process.env.PORT || 3000;
+app.listen(port)
 
-console.log('server started, listening on port 3000')
+console.log('server started, listening on port ' + port)
